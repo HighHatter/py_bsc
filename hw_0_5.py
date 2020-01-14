817ae9bf4ae082fd344b6f551c8576a6baad47e1
HighHatter <59743409+HighHatter@users.noreply.github.com>
2020-01-14 18:46:41 +0200

    Add files via upload

---
+++ hw_0_5.py
@@ -0,0 +1,6 @@
+input('Input side A: ')
+input('Intut side B: ')
+a=float(input('Input side A: '))
+b=float(input('Intut side B: '))
+c=a*b
+print('The area of recktangle equals: ', c)
---
+++ hw_0_7.py
@@ -0,0 +1,4 @@
+r=float(input('Input raius: '))
+pi=3.14
+S=pi*r^2
+print('The area of a circle equals: ', S)
