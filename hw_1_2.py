4c0b45615f26e8563aaed2d94e57b5ea497a5626
HighHatter <59743409+HighHatter@users.noreply.github.com>
2020-01-20 11:47:08 +0200

    Add files via upload

---
+++ hw_1_2.py
@@ -0,0 +1,4 @@
+a = float(input('Enter 1-st number: '))
+b = float(input('Enter 2-nd number: '))
+print('a / b = ', a / b, 'a // b = ', a // b, 'a * b = ', a * b, 'a + b = ', a + b, 'a - b = ', a - b, 'a % b = ',
+      a % b, 'a ** b = ', a**b)
---
+++ hw_1_3.py
@@ -0,0 +1,6 @@
+a = int(input('Enter integer number please: '))
+print('В вещественном прдеставлении:', float(a), 'В логическом представлении:', bool(a), "В сточном представлении:",
+      str(a))
+b = int(input('Enter another integer number please: '))
+print('float(a) + float(b) = ', float(a)+float(b), ' float(a) * float(b) = ', float(a)*float(b), ' str(a) + str(b) = ',
+      str(a)+str(b), ' bool(a) + bool(b) = ', bool(a)+bool(b), ' bool(a) * bool(b) = ', bool(a)*bool(b))
